53736f255878814ec707d4a509be6dc653c1df0c
davie khonje <daviekhonje@Davie-MacBook-Pro.local>
2016-07-27 17:48:31 +0100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
-var gulp = require('gulp'),
-	gutil = require('gulp-util');
 
-gulp.task('log',function() {
-	gutil.log('Workflows are owesome');
+
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
+
+var coffeeSources = ['components/coffee/*.coffee']
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+	.pipe(coffee({ bare: true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 
 });
